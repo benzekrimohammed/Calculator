866c87c398ec3e68355c548f9ba2d994f0321b0c
medbenzekri <medbenzekri219@gmaim.com>
2021-04-15 23:40:27 +0100

    fonctionless calculator !

---
+++ calculator/Controller.java
@@ -0,0 +1,4 @@
+package calculator;
+
+import javafx.scene.control.*;
+
---
+++ calculator/Main.java
@@ -0,0 +1,21 @@
+package calculator;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class Main extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage)  throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Calculator");
+        stage.show();
+    }
+}
