2411377a65c923b28a0f0a32094bca2476959098
benzekrimohammed <benzekri.mohammed@univ-bechar.dz>
2021-04-17 12:44:09 +0100

    working

--- calculator/Controller.java
+++ calculator/Controller.java
@@ -1,4 +1,16 @@
 package calculator;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.control.*;
 
+public class Controller {
+    
+   public Label panel;
+ public void insert(Event e){
+    Button button =  (Button)e.getSource();
+    String number= button.getText();
+    panel.setText(panel.getText()+number);     
+    
+ }
+}
