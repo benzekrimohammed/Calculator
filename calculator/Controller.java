71bdf865c680ce5af7b347e24e133eee8688a457
benzekrimohammed <benzekri.mohammed@univ-bechar.dz>
2021-04-20 01:03:28 +0100

    added 1 case of ÷ not tested yet inserting numbers has a bug

--- calculator/Controller.java
+++ calculator/Controller.java
@@ -1,8 +1,9 @@
 package calculator;
-
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Controller {
     
@@ -10,7 +11,24 @@ public class Controller {
  public void insert(Event e){
     Button button =  (Button)e.getSource();
     String number= button.getText();
+    if(panel.getText()=="0"){ 
+       panel.setText("");
+    }
     panel.setText(panel.getText()+number);  
-    
+ }
+ public void calculate(){
+ Pattern numbers = Pattern.compile("(\\d+)([+-\\*])+");
+ Matcher phrase = numbers.matcher(panel.getText());
+ double result=0;
+ for(int i=0;i<phrase.groupCount();i+=2){
+    switch(phrase.group(i)){
+       case "÷": 
+       result += Float.parseFloat(phrase.group(i-1))/Float.parseFloat(phrase.group(i+1));
+       System.out.println(result);
+       break;
+    }
+ }
+ 
+ 
  }
 }
