cdba062e4e31ae7ba9d921f7b2b8489855845ec5
benzekrimohammed <benzekri.mohammed@univ-bechar.dz>
2021-04-25 00:22:20 +0100

    key events but not totaly working . still needs : 1-stop letters from being accept as input 2- fix indentation when opration charachters are pressed 3- when enter button is clicked it doesn't triger the calculate button it insted triger the current foucesed button

--- calculator/Main.java
+++ calculator/Main.java
@@ -10,10 +10,16 @@ public class Main extends Application {
     }
     @Override
     public void start(Stage stage)  throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("calculator.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Calculator :)");
+        scene.setOnKeyPressed(e -> {
+            controller.keypressed(e);
+        });
         stage.show();
     }
 }
