9dcf1e1ad6ddb64bb8b03b07acc626a626129184
benzekrimohammed <benzekri.mohammed@univ-bechar.dz>
2021-04-28 02:38:55 +0100

    fixed key pressed

--- calculator/Controller.java
+++ calculator/Controller.java
@@ -11,7 +11,8 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class Controller {
-
+   Pattern oprators = Pattern.compile("[*X%\\+/-]");
+   Matcher matcher;
    @FXML
    private Label panel;
    @FXML
@@ -23,6 +24,8 @@ public class Controller {
    }
    
    public void insertop(String operator){
+      
+      if(operator.equals("*")) operator="X";
       panel.setText(panel.getText() + " " + operator + " ");
    }
    public Label getexpression(){
@@ -42,8 +45,7 @@ public class Controller {
    public void insert(Event e){
       Button button =  (Button)e.getSource();
       String btext = button.getText();
-      Pattern oprators = Pattern.compile("[X%\\+/-]");
-      Matcher matcher = oprators.matcher(btext);
+      matcher = oprators.matcher(btext);
       if(matcher.find()){ insertop(btext);return;};
       if(btext=="AC") return;
       panel.setText(panel.getText()+btext);     
@@ -56,8 +58,9 @@ public class Controller {
    }  
    public void keypressed (KeyEvent e){
    if(e.getCode()==KeyCode.BACK_SPACE) delete();
-   else if(e.getCode()==KeyCode.ENTER) calculate();
-   else panel.setText(panel.getText()+e.getText());
+   if(e.getCode()==KeyCode.ENTER) { calculate();e.consume(); }
+   if(e.getCode().isDigitKey()) panel.setText(panel.getText()+e.getText());
+   if(oprators.matcher(e.getText()).find()) insertop(e.getText());
    }
 }
 
